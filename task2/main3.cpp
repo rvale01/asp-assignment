d128481462dc63682fc2960ea5dc6e6e7be3a2d4
Valentina Ronchi (Student) <valentina2.ronchi@live.uwe.ac.uk@csctcloud.prxhn32zsyjupl12zde3wlfkch.cwx.internal.cloudapp.net>
2023-01-09 13:53:12 +0000

    removed useless file

--- task2/main3.cpp
+++ task2/main3.cpp
@@ -9,7 +9,7 @@ Scheduler s;
 
 void func1(){
     std::cout << "fiber 1" << std::endl;
-    auto dp = s.get_current().get_data();
+    auto dp = s.get_current()->get_data();
     std::cout << "fiber 1:" << *(int*)dp << std::endl;
     *(int*)dp += 1;
     s.fiber_exit();
@@ -17,7 +17,7 @@ void func1(){
 
 void func2(){
     std::cout << "fiber 2" << std::endl;
-    auto dp = s.get_current().get_data();
+    auto dp = s.get_current()->get_data();
     std::cout << "fiber 2:" << *(int*)dp << std::endl;
     s.fiber_exit();
 }
